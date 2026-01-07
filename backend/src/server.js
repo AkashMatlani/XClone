050ab9303be15b523d64c1ee3cd3d638a4212af3
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-07 12:47:59 +0000

    Start Server func added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
-import express from "express"
+import express from "express";
 import { connctDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
 
+const app = express();
 
-const app= express();
+const startServer = async () => {
+  try {
+    await connctDB();
+    app.listen(ENV.PORT, () =>
+      console.log("Server is up and running on PORT:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
 
-connctDB();
-
-app.listen(ENV.PORT ,()=> console.log("Server is up and running on PORT:",ENV.PORT));
+startServer();

febc7c3df8bbcb904a38f2c351b54f381c134275
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-08 14:29:08 +0000

    Update Profile API Call Added

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,8 +1,20 @@
 import asyncHanler from "express-async-handler";
 import User from "../models/user.model.js";
+import { getAuth } from "@clerk/express";
 export const getUserProfile = asyncHanler(async (req, res) => {
   const { userName } = req.params;
   const user = await User.findOne({ userName });
+  if (!user) return res.status(404).json({ error: "User not found" });
+
+  res.status(200).json({ user });
+});
+
+export const updateProfile = asyncHanler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const user = await User.findOneAndUpdate({ clerkId: userId }, req.body, {
+    new: true,
+  });
+
   if (!user) return res.status(404).json({ error: "User not found" });
 
   res.status(200).json({ user });

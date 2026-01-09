053f12c03a47f75a6ac98d2fcc39fc8dc7a163a7
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-09 12:19:50 +0000

    Sync User func created

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import asyncHanler from "express-async-handler";
 import User from "../models/user.model.js";
-import { getAuth } from "@clerk/express";
+import { clerkClient, getAuth } from "@clerk/express";
 export const getUserProfile = asyncHanler(async (req, res) => {
   const { userName } = req.params;
   const user = await User.findOne({ userName });
@@ -18,4 +18,33 @@ export const updateProfile = asyncHanler(async (req, res) => {
   if (!user) return res.status(404).json({ error: "User not found" });
 
   res.status(200).json({ user });
+});
+
+export const syncUser = asyncHanler(async (req, res) => {
+  const { userId } = getAuth(req);
+
+  //check if user alreday Exist in mongodb
+
+  const existingUser = await User.findOne({ clerkId: userId });
+  if (existingUser) {
+    return res
+      .status(200)
+      .json({ user: existingUser, message: "User alreday Exist" });
+  }
+
+  //Create new User from Clerk data
+  const clerkUser = await clerkClient.users.getUser(userId);
+
+  const userData = {
+    clerkId: userId,
+    email: clerkUser.emailAddresses[0].emailAddress,
+    firstName: clerkUser.firstName || "",
+    lastName: clerkUser.lastName || "",
+    userName: clerkUser.emailAddresses[0].emailAddress.split("@")[0],
+    profilePicture: clerkUser.imageUrl || "",
+  };
+
+  const user = await User.create(userData);
+
+  res.status(201).json({ user, message: "User Created successfully" });
 });
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,7 +1,16 @@
 import express from "express";
-import { getUserProfile } from "../controllers/user.controller.js";
+import { getUserProfile, syncUser } from "../controllers/user.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { updateProfile } from "../controllers/user.controller.js";
 
 const router = express.Router();
 router.get("/profile/:userName", getUserProfile);
+
+//saving data to DB
+router.post("/sync", protectRoute, syncUser);
+
+//update profile
+//check protectRoute for Authenticated and next function called for updateProfile
+router.put("/profile", protectRoute, updateProfile);
 
 export default router;

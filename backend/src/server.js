07cb34c6b40f2c55c831ac5eae2f2ebeeaa5faf1
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-14 12:41:43 +0000

    Tiny Fixes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,31 @@
 import express from "express";
 import { connctDB } from "./config/db.js";
 import { ENV } from "./config/env.js";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
 
 const app = express();
+
+//cors
+app.use(cors());
+
+//for handle req.body
+app.use(express.json());
+
+//Handle Authentication
+app.use(clerkMiddleware());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+
+//error handling middleware
+app.use((err, req, res,next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: err.message || "Internal server error" });
+});
 
 const startServer = async () => {
   try {

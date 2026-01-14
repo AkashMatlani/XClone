58251046d675e53b9f695db4958a18ffb815551b
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-14 13:21:22 +0000

    Create Comment API Call Added

--- backend/src/controllers/comment.controller.js
+++ backend/src/controllers/comment.controller.js
@@ -1,5 +1,9 @@
 import asyncHandler from "express-async-handler";
 import Comment from "../models/comment.model";
+import { getAuth } from "@clerk/express";
+import User from "../models/user.model";
+import Post from "../models/post.model";
+import Notification from "../models/notification.model";
 
 export const getComments = asyncHandler(async (req, res) => {
   const { postId } = req.params;
@@ -9,4 +13,45 @@ export const getComments = asyncHandler(async (req, res) => {
     .populate("user", "username firstname lastname profilePicture");
 
   res.status(200).json({ comments });
+});
+
+export const createComment = asyncHandler(async (req, res) => {
+  const { userId } = getAuth(req);
+  const { postId } = req.params;
+  const { content } = req.body;
+
+  if (!content || content.trim() === "") {
+    res.status(400).json({ error: "Comment content is required" });
+  }
+
+  const user = await User.findOne({ clerkId: userId });
+  const post = await Post.findById(postId);
+
+  if (!user || !post)
+    return res.status(404).json({ error: "User or Post not found" });
+
+  const comment = await Comment.create({
+    user: user._id,
+    post: postId,
+    content,
+  });
+
+  //link comment to the post
+  await Post.findByIdAndUpdate(postId, {
+    $push: { comments: comment._id },
+  });
+
+  //create notification if not connecting on own post
+
+  if (post.user.toString() !== user._id.toString()) {
+    await Notification.create({
+      from: user._id,
+      to: post.user,
+      type: "comment",
+      post: postId,
+      comment: comment._id,
+    });
+  }
+
+  res.status(201).json({ comment });
 });
--- backend/src/routes/comment.route.js
+++ backend/src/routes/comment.route.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { getComments } from "../controllers/comment.controller.js";
+import { getComments ,createComment} from "../controllers/comment.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 //public route
 
 router.get("/post/:postId", getComments);
+
+
+//protected route
+
+router.post("/post/:postId",protectRoute,createComment)
 
 export default router;

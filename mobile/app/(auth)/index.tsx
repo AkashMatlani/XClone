b45ca17f779f9f273d7bc9daacfd11836a46e067
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-20 12:30:03 +0000

    Google Button added

--- mobile/app/(auth)/index.tsx
+++ mobile/app/(auth)/index.tsx
@@ -1,4 +1,4 @@
-import { Image, View } from "react-native";
+import { Image, TouchableOpacity, View, Text } from "react-native";
 
 export default function Index() {
   return <View className="flex-1 bg-white">
@@ -11,6 +11,30 @@ export default function Index() {
             resizeMode="contain"></Image>
         </View>
 
+        <View className="flex-col gap-2">
+          {/* Google button */}
+          <TouchableOpacity
+            className="flex-row items-center justify-center bg-white border border-gray-300  rounded-full py-3 px-6"
+            onPress={() => { }}
+            style={{
+              shadowColor: "#000",
+              shadowOffset:{width:0,height:1},
+              shadowOpacity:0.1,
+              shadowRadius:2,
+              elevation:2, //only in android
+            }}
+          >
+            <View className="flex-row items-center justify-center">
+              <Image source={require("../../assets/images/google.png")}
+                className="size-10 mr-3"
+                resizeMode="contain"></Image>
+
+              <Text className="text-black font-medium text-base">Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+
+
+        </View>
       </View>
     </View>
   </View>

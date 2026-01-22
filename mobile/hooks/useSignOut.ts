0cc69706b3bcdee6db09e470cd740c147f0c322b
Akash Matlani <aakash.matlani@yahoo.com>
2026-01-22 12:21:45 +0000

    Custom Hook Added For SignUp Button

---
+++ mobile/hooks/useSignOut.ts
@@ -0,0 +1,22 @@
+import { useClerk } from "@clerk/clerk-expo"
+import { Alert } from "react-native";
+
+
+export const useSignOut = () => {
+    const { signOut } = useClerk();
+
+    const handleSignOut = () => {
+        Alert.alert("LogOut", "Are you sure want to Logout?", [
+            {
+                text: "Cancel",
+                style: "cancel"
+            },
+            {
+                text: "Logout",
+                style: "destructive",
+                onPress: () => signOut(),
+            }
+        ])
+    }
+    return { handleSignOut };
+}

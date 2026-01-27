c1df186220ea599e5217bd637618b1992ebe56ea
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-01-27 12:57:44 +0000

    QueryClient Configration Added For Sync

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -3,10 +3,17 @@ import { Stack } from 'expo-router'
 import { tokenCache } from '@clerk/clerk-expo/token-cache'
 import "../global.css"
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return <ClerkProvider tokenCache={tokenCache}>
-    <Stack screenOptions={{headerShown:false}}>
-      <Stack.Screen name='(auth)' options={{ headerShown: false }}></Stack.Screen>
-    </Stack>
+    <QueryClientProvider client={queryClient}> 
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name='(auth)' options={{ headerShown: false }}></Stack.Screen>
+      </Stack>
+    </QueryClientProvider>
+
   </ClerkProvider>
 }

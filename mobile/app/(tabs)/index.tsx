3b904d056e4ae89cb121d67cfd1f16b1f2500ea0
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-01-28 10:15:43 +0000

    Home Tab Header Added

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,14 +1,20 @@
 // Home Screen
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
+import { useUserSync } from '@/hooks/useUserSync'
+import { Ionicons } from '@expo/vector-icons'
 
 const HomeScreen = () => {
+    useUserSync();
     return (
         <SafeAreaView className='flex-1'>
-            <Text>HomeScreen</Text>
-            <SignOutButton/>
+            <View className='flex-row justify-between px-4 py-3 border-b border-gray-100'>
+                <Ionicons name='logo-twitter' size={24} color='#1DA1F2' />
+                <Text className='text-xl font-bold text-gray-900'>Home</Text>
+                <SignOutButton />
+            </View>
         </SafeAreaView>
     )
 }

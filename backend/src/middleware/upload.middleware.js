04694daa928042d09103eb4897f2e5abea7cc199
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-02-02 13:04:57 +0000

    File Uploading func modified

--- backend/src/middleware/upload.middleware.js
+++ backend/src/middleware/upload.middleware.js
@@ -1,11 +1,11 @@
-//multer is a middleware for hanlding multipart/form-data, which is primarily used for file Uploads
+// multer is a middleware for handling multipart/form-data, which is primarily used for file uploads
 
 import multer from "multer";
 
 const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("/image")) {
+  if (file.mimetype.startsWith("image/")) {
     cb(null, true);
   } else {
     cb(new Error("Only image files are allowed"), false);
@@ -15,7 +15,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB limit
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
 });
 
 export default upload;

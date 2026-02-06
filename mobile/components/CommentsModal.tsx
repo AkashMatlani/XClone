d93e9e3006079b1aee8e58e19e410891cf2cc443
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-02-06 12:31:00 +0000

    Comments Modal Header Section Added

--- mobile/components/CommentsModal.tsx
+++ mobile/components/CommentsModal.tsx
@@ -1,21 +1,35 @@
-import { View, Text } from 'react-native'
+import { View, Text, Modal, TouchableOpacity } from 'react-native'
 import React, { use } from 'react'
 import { Post } from '@/types';
 import { useComments } from '@/hooks/useComments';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
 
 
 interface CommentsModalProps {
     seletedPost: Post;
     onClose: () => void;
 }
-const CommentsModal = ({seletedPost, onClose}: CommentsModalProps) => {
+const CommentsModal = ({ seletedPost, onClose }: CommentsModalProps) => {
 
-    const {comentText,setCommentText,createComment,isCreatingComment} = useComments();
-  return (
-    <View>
-      <Text>CommentsModal</Text>
-    </View>
-  )
+    const { comentText, setCommentText, createComment, isCreatingComment } = useComments();
+
+    const { currentUser } = useCurrentUser();
+
+    const handleClose = () => {
+        setCommentText('');
+        onClose();
+    }
+    return (
+        <Modal visible={!!seletedPost} animationType='slide' presentationStyle='pageSheet'   >
+            <View className='flex-row items-center justify-between px-4  py-3 border-b border-gray-100'>
+                <TouchableOpacity onPress={handleClose}>
+                    <Text className='text-blue-500 text-lg'>Close</Text>
+                </TouchableOpacity>
+                <Text className='text-lg font-semibold'>Comments</Text>
+                <View className='w-12'></View>
+            </View>
+        </Modal>
+    )
 }
 
 export default CommentsModal

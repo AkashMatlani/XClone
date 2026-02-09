84d3d89210cfa4930eb3007caa464a6198cbccf3
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-02-09 11:57:17 +0000

    Comment Detail pag setup with Image and Content Data

--- mobile/components/CommentsModal.tsx
+++ mobile/components/CommentsModal.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Modal, TouchableOpacity } from 'react-native'
+import { View, Text, Modal, TouchableOpacity, ScrollView, Image } from 'react-native'
 import React, { use } from 'react'
 import { Post } from '@/types';
 import { useComments } from '@/hooks/useComments';
@@ -20,14 +20,47 @@ const CommentsModal = ({ seletedPost, onClose }: CommentsModalProps) => {
         onClose();
     }
     return (
-        <Modal visible={!!seletedPost} animationType='slide' presentationStyle='pageSheet'   >
+        <Modal visible={!!seletedPost} animationType='slide' presentationStyle='pageSheet'>
+            {/* Modal Header */}
             <View className='flex-row items-center justify-between px-4  py-3 border-b border-gray-100'>
                 <TouchableOpacity onPress={handleClose}>
                     <Text className='text-blue-500 text-lg'>Close</Text>
                 </TouchableOpacity>
                 <Text className='text-lg font-semibold'>Comments</Text>
-                <View className='w-12'></View>
+                <View className='w-12'>
+                </View>
             </View>
+              {seletedPost && (
+                    <ScrollView className='flex-1'>
+                        {/* Orignal Post */}
+                        <View className='border-b border-gray-100 bg-white p-4'>
+                            <View className='flex-row'>
+                                <Image source={{ uri: seletedPost.user.profilePicture }}></Image>
+                                <View className='flex-1' >
+                                    <View className='flex-row items-center mb-1'>
+                                        <Text className='font-bold text-gray-900 mr-1'>{seletedPost.user.firstName}
+                                            {seletedPost.user.lastName}
+                                        </Text>
+                                        <Text className='text-gray-500 ml-1'>@{seletedPost.user.username}</Text>
+                                    </View>
+
+                                    {seletedPost.content && (
+                                        <Text className='text-gray-900 text-base leading-5 mb-3'>
+                                            {seletedPost.content}
+                                        </Text>
+                                    )}
+
+                                    {seletedPost.image && (
+                                        <Image source={{ uri: seletedPost.image }}
+                                            className='w-full h-48 rounded-2xl mb-3' resizeMode='cover'>
+                                                
+                                            </Image>
+                                    )}
+                                </View>
+                            </View>
+                        </View>
+                    </ScrollView>
+                )}
         </Modal>
     )
 }

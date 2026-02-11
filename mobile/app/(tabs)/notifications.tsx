3c8153877fa8c93978afb67e1f1d8fe209148759
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-02-11 10:55:24 +0000

    Refetch API and UI Handled

--- mobile/app/(tabs)/notifications.tsx
+++ mobile/app/(tabs)/notifications.tsx
@@ -1,8 +1,23 @@
-import { Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { Text, TouchableOpacity, View } from 'react-native'
+import React, { use } from 'react'
+import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context'
+import { useNotification } from '@/hooks/useNotification';
 
 const NotificationScreen = () => {
+
+    const { notifications, isLoading, error, refetch, isRefetching, deleteNotification, isDeletingNotification } = useNotification();
+    const insets = useSafeAreaInsets();
+
+    if (error) {
+        return (
+            <View className='flex-1 items-center justify-center p-8'>
+                <Text className='text-gray-500'>Failed to load notifications</Text>
+                <TouchableOpacity onPress={() => refetch()} className='bg-blue-500  px-4 py-2 rounded-lg'>
+                    <Text className='text-white font-semibold'>Retry</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
     return (
         <SafeAreaView className='flex-1'>
             <Text>NotificationScreen</Text>

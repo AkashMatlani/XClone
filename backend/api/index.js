346bd7d12080246e0674f4555ac154445973f5b7
Akash Matlani <47881504+AkashMatlani@users.noreply.github.com>
2026-02-17 12:27:34 +0000

    index file updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,18 @@
+import serverless from "serverless-http";
 import app from "../src/server.js";
 import { connectDB } from "../src/config/db.js";
 
 let dbPromise = null;
+
+// Wrap Express app
+const handler = serverless(app);
 
 export default async (req, res) => {
   try {
     if (!dbPromise) dbPromise = connectDB(); // cache Mongo connection
     await dbPromise;
 
-    app(req, res);
+    return handler(req, res); // use serverless-http wrapper
   } catch (error) {
     console.error("Serverless function error:", error.stack);
     res.status(500).json({ error: "Internal server error" });
